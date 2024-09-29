29f2d71dfd0dd85df57d3daca75fe4b1a47df89c
Kamil Ramocki <kamil.ramocki@gmail.com>
2024-09-29 03:21:44 +0200

    Dodałem bezużyteczne gówno

--- endpoints/companies.js
+++ endpoints/companies.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const { Company } = require('../models/company');
+const {Review} = require("../models/review");
+const {analyzeReviews} = require("../ai");
 const companiesRouter = express.Router();
 
 companiesRouter.get('/', async (req, res) => {
     const company = await Company.find({});
+    res.json(company)
+});
+
+companiesRouter.get('/:nip', async (req, res) => {
+    const { nip } = req.params;
+    const company = await Company.findOne({ nip });
+
     res.json(company)
 });
 
