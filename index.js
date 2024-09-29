e675df157ca5748100424d42ab8fbdb604f8f9eb
olafsozanski <olaf.sozanski@gmail.com>
2024-09-29 06:18:43 +0200

    I love CORS

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.set('Access-Control-Allow-Origin', '*');
     res.set('Access-Control-Allow-Method', 'GET,POST,PUT');
+    res.set('Access-Control-Allow-Headers', '*');
     next();
 });
 
